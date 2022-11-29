b6e13b34179de878d6b22d7edcd521f92ec3c5ff
shraddha-cpu <shraddhapaygude5337@gmail.com>
2022-11-29 14:35:36 +0530

    Add project files.

---
+++ EFDemp/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFDemp.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+            [Key]
+            [ScaffoldColumn(false)]
+            public int Id { get; set; }
+
+            [Required(ErrorMessage = "Name is required")]
+            public string? Name { get; set; }
+
+            [Required(ErrorMessage = "Company Name is required")]
+            [Display(Name = "Company Name")]
+            public string? CompanyName { get; set; }
+
+            [Required(ErrorMessage = "Price is required")]
+            public decimal Price { get; set; }
+        }
+    }
+
+
---
+++ EFDemp/Models/ProductCRUD.cs
@@ -0,0 +1,50 @@
+﻿namespace EFDemp.Models
+{
+    public class ProductCRUD
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductCRUD(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products.ToList();
+        }
+        public Product GetProductById(int id)
+        {
+            var product = _context.Products.Where(x => x.Id == id).FirstOrDefault();
+            if (product != null)
+                return product;
+            else
+                return null;
+
+        }
+        public int AddProduct(Product prod)
+        {
+            _context.Products.Add(prod);
+            int res = _context.SaveChanges();
+            return res;
+
+        }
+        public int UpdateProduct(Product prod)
+        {
+            _context.Products.Update(prod);
+            int res = _context.SaveChanges();
+            return res;
+        }
+        public int DeleteProduct(int id)
+        {
+            int res = 0;
+            var product = _context.Products.Where(x => x.Id == id).FirstOrDefault();
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                res=_context.SaveChanges();
+
+            }
+            return res;
+        }
+
+    }
+}
